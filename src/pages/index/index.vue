0948038ac5b91eef916e6c178bf0ca0ba525b381
hansking <im.hansking@gmail.com>
2019-07-13 12:01:29 +0800

    mpvue 框架搭建完成

---
+++ src/pages/index/index.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    卓聘网络科技 - 招聘系统
+  </div>
+</template>
+
+<script>
+import card from '@/components/card'
+
+export default {
+  data () {
+    return {
+
+    }
+  },
+
+  components: {
+    card
+  },
+
+  methods: {
+
+  },
+
+  created () {
+    // let app = getApp()
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
